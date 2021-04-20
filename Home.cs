7d3fb3ee339954b337460359362cb248542986c2
Bradley Grainger <bgrainger@gmail.com>
2021-04-19 21:50:26 -0700

    Implement cookie test functions.

--- Home.cs
+++ Home.cs
@@ -1,35 +1,52 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace FunctionsCookies
 {
     public static class Home
     {
         [FunctionName("Home")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            return new ContentResult
+            {
+                ContentType = "text/html",
+                Content = @"<!doctype html>
+<html>
+<head>
+  <meta charset='utf-8'>
+  <title>Static Web App Cookies</title>
+</head>
+<body>
+  <div>API output:</div>
+  <div id=output></div>
+  <script type='text/javascript'>
+let count = 0;
+function callApi() {
+  let url = '/api/Test';
+  if (++count % 3 == 0) {
+    url += '?setcookie=' + count;
+  }
+  fetch(url)
+  .then(response => response.text())
+  .then(text => {
+    var p = document.createElement('p');
+    p.innerText = text;
+    document.getElementById('output').appendChild(p);
+  });
+}
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+window.setInterval(callApi, 3000);
+  </script>
+</body>
+</html>",
+            };
         }
     }
 }
--- Test.cs
+++ Test.cs
@@ -1,35 +1,31 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace FunctionsCookies
 {
     public static class Test
     {
         [FunctionName("Test")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            var cookie = req.Cookies["test"];
+            var message = $"Request Cookie = {cookie}.";
 
-            string name = req.Query["name"];
+            var setcookie = req.Query["setcookie"];
+            if (setcookie.Count != 0)
+            {
+                req.HttpContext.Response.Cookies.Append("test", setcookie[0]);
+                message += $" Setting cookie to {setcookie[0]}.";
+            }
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            req.HttpContext.Response.Headers["Cache-Control"] = "no-store, max-age=0";
+            return new OkObjectResult(message);
         }
     }
 }
